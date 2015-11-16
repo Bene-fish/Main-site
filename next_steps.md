a253a7b2c667648e7c0d790236601d2794d9bc1a
hemangandhi <hemangandhi@gmail.com>
2015-11-16 13:14:15 -0500

    Added a need upload page and added to auth

---
+++ next_steps.md
@@ -0,0 +1,23 @@
+#Next Steps
+
+*Need pages to be added.
+*On auth success, send "add need" button's HTML and "sign out" button's HTMLand UID as JSON.
+*All logged in gets send UID as header.
+*If the UID is sent, JSON with HTML is returned.
+
+The site organization:
+```
+<html>
+<head>
+<!-- Scripts -->
+</head>
+<body>
+<div>
+<!-- top with sign in, out and add need -->
+</div>
+<div>
+<!-- relevant HTML as per needs (4th point on list) -->
+</div>
+</body>
+</html>
+```
--- srv.py
+++ srv.py
@@ -1,7 +1,53 @@
-#this is only to test the code for the webpage.
-#python 3 only.
+#! /usr/bin/env python3
 import http.server as srv
+import cgi
+#from http import HTTPStatus #find 3.4 equivalent...
+from oauth2client import client, crypt
 
-r = srv.HTTPServer(('localhost',8080),srv.SimpleHTTPRequestHandler)
+CLIENT_ID = "746271585204-8kcc5u48lf4q9u76dp32a45piardm6c5.apps.googleusercontent.com"
+
+def verify_token(token):
+  try:
+    idinfo = client.verify_id_token(token, CLIENT_ID)
+    if idinfo['aud'] != CLIENT_ID: #WEB_CLIENT_ID is CLIENT_ID...
+      raise crypt.AppIdentityError("Unrecognized client.")
+    if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+      raise crypt.AppIdentityError("Wrong issuer.")
+#    if idinfo['hd'] != APPS_DOMAIN_NAME: #maybe when we're legit...
+#      raise crypt.AppIdentityError("Wrong hosted domain.")
+  except crypt.AppIdentityError as e:
+    print("oauth2client error:",e)
+    return False
+  else:
+    return idinfo['sub']
+
+users = set() 
+
+class FishRequestHandler(srv.SimpleHTTPRequestHandler):
+  def do_GET(self):
+    if self.client_address[0] in ok_IPs:
+      self.send_header("Access-Control-Allow-Origin",'*')
+    srv.SimpleHTTPRequestHandler.do_GET(self)  
+  def do_POST(self):
+    ctype, p = cgi.parse_header(self.headers['Content-Type'])
+    if ctype == 'application/x-www-form-urlencoded':
+      l = int(self.headers['Content-Length'])
+      postvars = cgi.parse_qs(self.rfile.read(l), keep_blank_values = 1)
+      #postvars['id_token'] = postvars[b'id_token'][0].decode('utf-8')
+      ver = verify_token(postvars[b'id_token'][0])
+      if ver != False:
+        users.update(set(ver))
+        self.send_response(301)
+        self.send_header("Location","/need_upload.html")
+        self.send_header("Access-Control-Allow-Origin",'*')
+        self.end_headers()
+      else:
+        self.send_response(400)
+        self.end_headers()
+    else:
+      self.send_response(404)
+      self.end_headers()
+
+r = srv.HTTPServer(('127.0.0.1',8080),FishRequestHandler)
 
 r.serve_forever()
